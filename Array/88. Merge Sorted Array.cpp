9b916a423d590206db44fb810fb865a4c1401ae0
Sridip <stsdutta2@gmail.com>
2019-08-13 09:54:59 +0530

    Array

---
+++ Array/88. Merge Sorted Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       
+        nums1.erase(nums1.begin()+m,nums1.end());
+    
+        for(int i=0; i<n; ++i)
+        {
+            nums1.insert( lower_bound(nums1.begin(), nums1.end(), nums2[i]), nums2[i]);
+        }
+    }
+};
