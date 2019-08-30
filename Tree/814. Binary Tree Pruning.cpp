ba82d1797bc77dff90d7032eb63a741a21cbaef2
Sridip <stsdutta2@gmail.com>
2019-08-30 23:23:51 +0530

    Tree count:- 1

--- Tree/814. Binary Tree Pruning.cpp
+++ Tree/814. Binary Tree Pruning.cpp
@@ -7,27 +7,34 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    TreeNode* pruneTree(TreeNode* root) {
-        if (root == NULL) return NULL;
-        
+    TreeNode *pruneTree(TreeNode *root)
+    {
+        if (root == NULL)
+            return NULL;
+
         //l_dfs
         //r_dfs
         send(root);
         return root;
     }
-    
-    int send(TreeNode* root){
-        
-        if (root == NULL) return 0;
-        
+
+    int send(TreeNode *root)
+    {
+
+        if (root == NULL)
+            return 0;
+
         int l_dfs = send(root->left);
         int r_dfs = send(root->right);
-        
-        if (l_dfs < 1) root->left = NULL;
-        if (r_dfs < 1) root->right = NULL;
-        
-        return root->val+l_dfs+r_dfs;
+
+        if (l_dfs < 1)
+            root->left = NULL;
+        if (r_dfs < 1)
+            root->right = NULL;
+
+        return root->val + l_dfs + r_dfs;
     }
 };
