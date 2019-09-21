8ff67ad109f7ce30edde32fdd78d63a0c67cf65f
Sridip <stsdutta2@gmail.com>
2019-09-21 10:53:57 +0530

    Updated

---
+++ Array/11. Container With Most Water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        
+        // two pointer approach
+        int area = 0;
+        int left = 0, right = n-1;
+        
+        while(left < right)
+        {
+            area = max(area, min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right])
+            {
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return area;
+    }
+};
